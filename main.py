badf9d9f42dc1f503fb5e4c50ca581b263ef50f7
“QianFuXin” <Mr_Qian_Ives@163.com>
2022-04-19 10:00:05 +0800

    update .gitignore

--- main.py
+++ main.py
@@ -1,80 +1,68 @@
 # encoding:utf-8
 import time
-import logging
 import re
-from ast import literal_eval
-from requests.exceptions import ProxyError, ReadTimeout, ConnectTimeout
-from urllib3.exceptions import ReadTimeoutError
-from utils.webworm.HeaderAndProxies import *
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 import requests
 import pandas as pd
 import os
 import numpy as np
 import matplotlib as mpl
 
+# 设置字体
 mpl.rcParams['font.sans-serif'] = ['KaiTi']
 mpl.rcParams['font.serif'] = ['KaiTi']
 mpl.rcParams['axes.unicode_minus'] = False
 import matplotlib.pyplot as plt
 
-fileName = f'基金排行{time.strftime("%Y%m%d", time.localtime())}.txt'
-allData = []
 
-
+# 获取所有基金数据
 def getFundData():
+    # 文件名称
+    fileName = f'基金排行{time.strftime("%Y%m%d", time.localtime())}.txt'
+    # 存放所有的数据
+    allData = []
     pi = 1
-    stopTime = time.strftime("%Y-%m-%d", time.localtime())
-
-    beginTime = f"{int(stopTime[:4]) - 1}-01-01"
+    # 开始和结束时间
+    today = time.strftime("%Y-%m-%d", time.localtime())
     while 1:
         headers = {
-            'User-Agent': getRandomAgent(),
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
             'Referer': 'http://fund.eastmoney.com/data/fundranking.html'
         }
-        proxies = getRandomProxies()
         logging.info(f"爬取第{pi}页")
-        url = f"http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=all&rs=&gs=0&sc=qjzf&st=desc&sd={beginTime}&ed={stopTime}&qdii=&tabSubtype=,,,,,&pi={pi}&pn=50&dx=1"
+        url = f"http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=all&rs=&gs=0&sc=6yzf&st=desc&sd={today}&ed={today}&qdii=&tabSubtype=,,,,,&pi={pi}&pn=50&dx=1"
+        # 表示礼貌
+        time.sleep(3)
         try:
-            data = requests.get(url, headers=headers, proxies=proxies, timeout=3).text.strip()
-        except ProxyError:
-            logging.warning("代理无效")
-            continue
-        except (ReadTimeout, ConnectTimeout, ReadTimeoutError):
-            logging.warning("代理超时")
-            continue
+            tempData = requests.get(url, headers=headers, timeout=10).text.strip()
+            result = re.findall("\[.*\]", tempData)
+            if len(result) == 1 and not result[0] == [404]:
+                data = eval(result[0])
+                # 数据爬取完成，退出循环
+                if not data:
+                    break
+                logging.info(f"第{pi}页数据已爬取完成")
+            else:
+                logging.info("数据无效，重新爬取。")
+                continue
         except Exception  as e:
-            logging.error(f"其他错误{e}")
+            logging.error(f"发生了错误：{e}")
             continue
-        try:
-            data = re.findall("\[.*\]", data)[0]
-            temp = literal_eval(data)
-            logging.error(data[:20])
-        except:
-            logging.warning("数据不合规定")
-            continue
-        if temp == [404]:
-            logging.error("404数据，不符合规范")
-            continue
-        if not temp:
-            logging.info("数据已爬取完成")
-            break
         # 添加本页数据
-        allData.extend(temp)
+        allData.extend(data)
         pi += 1
-        with open(fileName, mode='w', encoding="utf-8") as  file:
-            file.write("\n".join(map(str, allData)))
+    # 保存数据到本地
+    with open(fileName, mode='w', encoding="utf-8") as  file:
+        file.write("\n".join(map(str, allData)))
 
 
+# 进行统计
 def recommendFund():
     """
-        月收益<3月收益
-        3月收益<6月收益
-        6月收益<1年收益
-        且本周+以上全红
-        视为稳定的基金
-        按照1年收益进行排序
-        然后把top10制图发送邮件给自己
-        脚本每月16号跑一次
+        一支好且稳定基金的标准
+        月收益<3月收益<6月收益<1年收益
     """
     columns = ['基金代码', '基金简称', '字符代码', '日期',
                '单位净值', '累计净值', '日增长率', '近1周',
@@ -83,49 +71,41 @@ def recommendFund():
                '成立来']
     df = pd.DataFrame([i.strip().split(",")[:16] for i in allData], columns=columns)
     df = df.set_index("基金代码")
-    # 去除空值
-    df.dropna()
-    # 将空白值替换为空值
+    # 删除一些列
+    del df["成立来"]
+    del df["今年来"]
+    del df["单位净值"]
+    del df["累计净值"]
+    del df["日增长率"]
+    del df["近3年"]
+    del df["近2年"]
+    # 将空白值替换为缺失值
     df.replace(to_replace=r'^\s*$', value=np.nan, regex=True, inplace=True)
-    # 清洗数据(去除垃圾数据)
-    df.query("单位净值==单位净值", inplace=True)
-    df.query("近1周==近1周", inplace=True)
-    df.query("近1月==近1月", inplace=True)
     # 填充数据（因为某些新生基金近1年、近2年为空，所以用其他列去填充）
-    df['近3月'].fillna(df['近1月'], inplace=True)
-    df['近6月'].fillna(df['近3月'], inplace=True)
     df['近1年'].fillna(df['近6月'], inplace=True)
-    df['近2年'].fillna(df['近1月'], inplace=True)
-    print(df.shape[0])
+    # 去除缺失值
+    df = df.dropna(subset=['日期', '近1月', '近1周', '近3月', '近6月'])
     # 更改类型
     df["近1周"] = df["近1周"].astype("float")
     df["近1月"] = df["近1月"].astype("float")
     df["近3月"] = df["近3月"].astype("float")
     df["近6月"] = df["近6月"].astype("float")
     df["近1年"] = df["近1年"].astype("float")
-    df["近2年"] = df["近2年"].astype("float")
     # 去除负数据
-    df.query("近1月>0", inplace=True)
-    print(df.shape[0])
-    df.query("近3月>0", inplace=True)
-    print(df.shape[0])
-    df.query("近6月>0", inplace=True)
-    print(df.shape[0])
-    df.query("近1年>0", inplace=True)
-    print(df.shape[0])
-    df.query("近2年>0", inplace=True)
+    df.query("(近1月>0) and (近3月>0) and (近6月>0) and  (近1年>0)", inplace=True)
     # 稳定基金查询
-    df.query("近3月>=近1月 and 近6月>=近3月 and 近1年>=近6月 and 近2年>=近1年", inplace=True)
-    print(df.shape[0])
-    df = df.sort_values(by="近1年", ascending=False)[:20]
+    df.query("(近3月>=近1月) and (近6月>=近3月) and (近1年>=近6月)", inplace=True)
+    # 按照近一年的标准进行排序
+    df = df.sort_values(by=["近1年"], ascending=False)[:20]
+    # 作图
     fig, ax = plt.subplots(figsize=(35, 40))
-    df[['近1月', '近3月', '近6月', '近1年',
-        '近2年']].plot.bar(subplots=True, ax=ax, rot=0)
+    df[['近1月', '近3月', '近6月', '近1年']].plot.bar(subplots=True, ax=ax, rot=0)
+    # 保存
     fig.savefig(f"{fileName}推荐.png", quality=10)
 
 
 if __name__ == '__main__':
-
+    # 优先使用本地数据，因为一天之内，这个数据几乎不会有改动。
     if os.path.exists(fileName):
         logging.info("导入本地数据")
         with open(fileName, mode='r', encoding="utf-8") as  file:
